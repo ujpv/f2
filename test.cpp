d7cc1aebc196650554f76d5e1cf65e12f910870a
Vladimir Kharitonov <khvv@yandex-team.ru>
2019-02-18 14:45:56 +0300

    clean in process logic

--- test.cpp
+++ test.cpp
@@ -15,18 +15,20 @@
 
 namespace  {
 
-// Limits
-constexpr size_t SPARKS_COUNT = 64;
-constexpr size_t MAX_POINTS = 2048 * SPARKS_COUNT;
-constexpr size_t MAX_EVENETS = 100;
-
 // Effect consts
+constexpr size_t SPARKS_COUNT = 64; // on one burst
 constexpr float MIN_START_SPEED = 0; // in (px / sec)
 constexpr float MAX_START_SPEED = 100; // in (px / sec)
 constexpr float GRAVIRY = -100.0; // in (px / sec ^ 2)
 constexpr int64_t MIN_LIFE_TIME = 1; // in sec
 constexpr int64_t MAX_LIFE_TIME = 2; // in sec
-constexpr float BURST_PROBABILITY = 2 /64.f; // 0..1
+constexpr float EFFECT_PROBABILITY = 3 /64.f; // 0..1
+
+// Limits
+constexpr size_t MAX_EFFECTS = 2048;
+constexpr size_t MAX_EVENTS = 100; // clicks evens queue size
+constexpr size_t MAX_POINTS = MAX_EFFECTS * SPARKS_COUNT;
+
 
 // Defenitions
 struct Color { float r, g, b, a; };
@@ -101,20 +103,24 @@ Color randomColor()
   return {colors[0], colors[1], colors[2], 1.f};
 }
 
-void makeBurst(Vec2f position, Buffer &out)
+void makeEffect(Vec2f position, Buffer &out)
 {
-  if (out.size() + SPARKS_COUNT >= MAX_POINTS)
-    return;
-
   const auto color = randomColor();
   int64_t timeNow = globalTimeMs.load();
   for (size_t i = 0; i < SPARKS_COUNT; ++i) {
+    assert(out.size() < MAX_POINTS);
     out.emplace_back(position,
                      Vec2f::makeRandom(MIN_START_SPEED, MAX_START_SPEED), // speed
                      timeNow,
                      randomValue(MIN_LIFE_TIME * 1000, MAX_LIFE_TIME * 1000), // lifetime ms
                      color);
   }
+}
+
+bool isInsideScene(const Vec2f& pos) {
+  return  (pos.x >= 0 && pos.y >= 0 &&
+           pos.x <= test::SCREEN_WIDTH &&
+           pos.y <= test::SCREEN_HEIGHT);
 }
 
 // Mechanic logic:
@@ -139,34 +145,36 @@ void WorkerThread(void)
 
     dstBuffer.clear();
 
-    for (const auto &spark: srcBuffer) {
-      auto x = spark.getPosition().x;
-      auto y = spark.getPosition().y;
-      if (x < 0 || y < 0 ||
-          x > test::SCREEN_WIDTH || y > test::SCREEN_HEIGHT)
-      {
-        continue;
+    // process exist sparks:
+    for (auto &spark: srcBuffer) {
+      if (isInsideScene(spark.getPosition()) && not spark.isExpired(time)) {
+        dstBuffer.push_back(spark);
+        dstBuffer.back().applayForce(float(delta), Vec2f{0, GRAVIRY});
+        dstBuffer.back().update(delta);
       }
-
-      if (spark.isExpired(time)) {
-        if (randomBinary(BURST_PROBABILITY))
-          makeBurst(spark.getPosition(), dstBuffer);
-
-        continue;
-      }
-
-      if (dstBuffer.size() >= MAX_POINTS)
-        break;
-
-      dstBuffer.push_back(spark);
-      dstBuffer.back().applayForce(float(delta), Vec2f{0, GRAVIRY});
-      dstBuffer.back().update(delta);
     }
 
+    // create new effects:
+    for (auto &spark: srcBuffer) {
+      if (dstBuffer.size() + SPARKS_COUNT >= MAX_POINTS)
+        break;
+
+      if (isInsideScene(spark.getPosition()) && spark.isExpired(time)) {
+        if (randomBinary(EFFECT_PROBABILITY))
+          makeEffect(spark.getPosition(), dstBuffer);
+      }
+    }
+
+    // hande click events:
     if (not clicks.empty()) {
       std::lock_guard<SpinLock> guard{clicksMutex};
-      for (auto& c: clicks)
-        makeBurst(c, dstBuffer);
+      for (auto& c: clicks) {
+        if (dstBuffer.size() + SPARKS_COUNT >= MAX_POINTS)
+          break;
+
+        makeEffect(c, dstBuffer);
+      }
+
       clicks.clear();
     }
 
@@ -180,7 +188,9 @@ void WorkerThread(void)
       if (not swapped)
         std::this_thread::yield();
     }
+
   } // while (!exitFlag)
+
 } // void WorkerThread(void)
 
 } // anonymous namespace
@@ -191,7 +201,7 @@ void test::init(void)
   for (auto& b: buffers)
     b.reserve(MAX_POINTS);
 
-  clicks.reserve(MAX_EVENETS);
+  clicks.reserve(MAX_EVENTS);
 
   std::thread workerThread(WorkerThread);
   workerThread.detach(); // Glut + MSVC = join hangs in atexit()
@@ -218,6 +228,7 @@ void test::render(void)
   for (size_t i = 0; i < buffers[activeBuffer].size(); ++i)
     buffers[activeBuffer][i].draw();
 
+//  std::cout << "Buffer size: " << buffers[activeBuffer].size() << std::endl;
   state.exchange(activeBuffer);
 }
 
@@ -231,5 +242,9 @@ void test::on_click(int x, int y)
 {
   y = int(SCREEN_HEIGHT) - y;
   std::lock_guard<SpinLock> lock(clicksMutex);
+
+  if (clicks.size() >= MAX_EVENTS)
+    return; // drop this event
+
   clicks.emplace_back(x, y);
 }
